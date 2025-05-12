dc6da42d117082cfa6021901164b3be4f4cf7ac1
Linitauwu <xombixc@gmail.com>
2025-05-11 19:46:01 -0600

    Tu mensaje de commit aquí

--- database.js
+++ database.js
@@ -2,9 +2,9 @@ const mysql = require('mysql2/promise');
 
 // Crear el pool de conexiones
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || 'localhost',         // Dirección del servidor, por defecto 'localhost'
+  host: process.env.DB_HOST || 'mysql.railway.internal',         // Dirección del servidor, por defecto 'localhost'
   user: process.env.DB_USER || 'root',             // Usuario por defecto 'root'
-  password: process.env.DB_PASSWORD || '',         // Contraseña (si está configurada)
+  password: process.env.DB_PASSWORD || 'MUPZXvExsjosBGlooTkSyzgNqasQRdMU',         // Contraseña (si está configurada)
   database: process.env.DB_NAME || 'tutores',      // Nombre de la base de datos
   port: process.env.DB_PORT || 3306,               // Puerto por defecto 3306
   waitForConnections: true,                         // Esperar conexiones disponibles
