08a8a03581189f8c8e8a919bc92e5d63925d3589
Linitauwu <xombixc@gmail.com>
2025-05-11 20:13:39 -0600

    Actualización de la conexión a la base de datos

--- database.js
+++ database.js
@@ -1,16 +1,15 @@
-require('dotenv').config();
-
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-  host: 'containers-us-west-144.railway.app',  // El dominio público (verifica que sea correcto)
-  user: 'root',
-  password: 'MUPZXvExsjosBGlooTkSyzgNqasQRdMU',
-  database: 'railway',
-  port: 3306,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT || 3306,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
 });
 
 module.exports = pool;
+
